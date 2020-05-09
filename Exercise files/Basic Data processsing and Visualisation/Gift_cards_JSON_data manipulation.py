d17177c8d7abbc47f65cba0423e27e6ab5ad7476
nikhilpolpakkara <63650218+nikhilpolpakkara@users.noreply.github.com>
2020-05-10 05:29:10 +0530

    Data product: Week 2
    Running stats on datasets
    creating
    1. defaultdict
    2. list of dict
    3. list of tuples
    sorting, taking average etc

--- Exercise files/Basic Data processsing and Visualisation/Gift_cards_JSON_data manipulation.py
+++ Exercise files/Basic Data processsing and Visualisation/Gift_cards_JSON_data manipulation.py
@@ -1,6 +1,9 @@
 #Created by: Nikhil Polpakkara
 import json
 import ast
+import numpy as np
+from collections import defaultdict
+
 path = "M:/Git repository/Python/JSON/Gift_Cards.json"
 f = open(path,'r',encoding='utf-8')
 j = json.loads(f.readline())
@@ -34,3 +37,40 @@ for i in range(0,len(lst)):
 dataset = []
 for i in range(1000):
     dataset.append(json.loads(f.readline()))
+
+#%%
+#Calculating mean from the data
+sum_ = 0    
+for i in range(1000):
+    a = dataset[i]['overall']
+    sum_ = sum_ + a
+mean = sum_/len(dataset)
+print(mean)
+
+for i in range(1000):
+    mean = np.mean(dataset[i]['overall'])
+#%%
+#making a list of overall ratings
+di = []
+for d in dataset:
+    a = d.get('overall')
+    di.append(a)
+
+#%%
+# dictionary of overall ratings
+rate_count = {1:0,2:0,3:0,4:0,5:0}
+for i in range(1000):
+    rate_count[dataset[i]['overall']] += 1
+rate_count
+#%%
+#Counting verified purchases
+verifiedcounts = defaultdict(int)
+for d in dataset:
+    verifiedcounts[d['verified']] += 1
+#%%
+#popular product id
+Prod_popular = defaultdict(int)
+for d in dataset:
+    Prod_popular[d['asin']] += 1
+sort_list = [(Prod_popular[p],p) for p in Prod_popular]
+sort_list = sorted(sort_list, key=lambda x: x[0], reverse=True)
--- Exercise files/Basic Data processsing and Visualisation/yelp_dataset_practice.py
+++ Exercise files/Basic Data processsing and Visualisation/yelp_dataset_practice.py
@@ -2,12 +2,48 @@
 #Created by: Nikhil Polpakkara
 import json
 import ast
+from collections import defaultdict
+from datetime import datetime
+
 path = "M:/Dataset/yelp_dataset.json"
 f = open(path,'r', encoding='utf-8')
 
+#cnt = 0 
+#for line in f:
+    
 
 dataset = []
-for i in range(50):
-    dataset.append(json.loads(f.readline()))
 
-dataset[0]
+for i in range(100):
+    try:
+        dataset.append(json.loads(f.readline()))
+    except:
+        continue
+
+#%%
+#how to make a dictionary with list
+funny_name = defaultdict(list)
+
+for d in dataset:
+    funny_name[d['name']].append(d['funny'])
+#%%  
+#most longest yelping user
+for d in dataset:
+    a = d['yelping_since'].split()
+    t = datetime.fromisoformat(a[0]).timestamp()
+    current_date = datetime.fromisoformat('2020-05-10').timestamp()
+    diff = current_date - t
+#%%
+def count_num_friends(friends_list):
+    friends = list(friends_list.split())
+    num_friends = len(friends)
+    return num_friends
+
+friend_count = {}
+for d in dataset:
+    friends = d['friends']
+    friend_count[d['name']] = count_num_friends(friends)
+
+friends_count = [(friend_count[p],p) for p in friend_count]
+friends_count.sort()
+Top_friends = friends_count[-10:]
